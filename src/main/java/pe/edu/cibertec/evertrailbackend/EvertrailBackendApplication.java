215f1f556f9d774a850d591484702519d4017bc7
Daniel Isidro <isidroesteban402@gmail.com>
2024-09-30 23:19:14 -0500

    creando proyecto backend

---
+++ src/main/java/pe/edu/cibertec/evertrailbackend/EvertrailBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.evertrailbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvertrailBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvertrailBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/cibertec/evertrailbackend/EvertrailBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.evertrailbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvertrailBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
